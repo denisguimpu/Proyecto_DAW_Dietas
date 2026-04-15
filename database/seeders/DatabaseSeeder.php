a93b8fbf8e71ffdf82ea9f8f5594681570c47450
denisguimpu <denisalinalarisa@gmail.com>
2026-04-15 22:02:15 +0200

    feat: rename calories column to kcal and initialize DatabaseSeeder with default user and ingredients

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,11 +4,22 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Database\Seeders\IngredientSeeder;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
+        User::firstOrCreate(
+            ['email' => 'denisalinalarisa@gmail.com'],
+            [
+                'name' => 'denis',
+                'password' => '$2y$12$Zwgh3XZAPlTZUFDc5a7r1uz8hue4y4FNNWs5CAnFj9csrSn6t6pVa',
+                'remember_token' => '0unDFJW21KttXtlPcdFSTgJT8XsAgOEFahmRucwHY8tpCOdTjXUt65n1I9sn',
+                'email_verified_at' => null,
+            ]
+        );
+
         $this->call([
             IngredientSeeder::class,
         ]);
